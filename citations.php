cbb2c48b7a4c5256fa7e9f58bddda1f0bdb6ff96
Kozodoi <n.kozodoi@icloud.com>
2021-02-25 16:01:15 +0100

    fix: Update citations.php

--- citations.php
+++ citations.php
@@ -1,30 +1,3 @@
-<style>
-html, body, form, table, div, h1, h2, h3, h4, h5, h6, img, ol, ul, li, button {
-    margin: 0px;
-    padding: 0px;
-    border: 0px none;
-    color: #fff;
-}
-
-table {
-    border-collapse: collapse;
-    border-width: 0px;
-    empty-cells: show;
-}
-
-body, td {
-    font-size: 13px;
-    font-family: Arial,sans-serif;
-    line-height: 1.24;
-}
-
-.gsc_g_hist_wrp
-{
-	padding-top: 10px !important;
-}
-
-</style>
-
 <?php
 	function filter_content($content, $url)
 	{
@@ -70,7 +43,7 @@ body, td {
 		}
 
 		$contentText2 = $dom->saveHTML();
-		$contentText2 = 'Citations according to <a href="'.$url.'">Google Scholar</a>: '
+		$contentText2 = 'Total citations on <a href="'.$url.'">Google Scholar</a>: '
                                 .$citations.' (h-index: '.$hindex.')'.$contentText2;
 
 		return $contentText2;
