aa3fbfe3a808a6dd1920034baac8ab72c6dbd71d
kozodoi <kozodoi@users.noreply.github.com>
2021-03-24 17:27:59 +0000

    deploy: e5d884baf46e62d80ad54bfee37bf0b6dfc4c982

---
+++ assets/js/paper_description.js
@@ -0,0 +1,79 @@
+function paper_abstract_1() {
+  var dots = document.getElementById("dots1");
+  var moreText = document.getElementById("abs1");
+  var btnText = document.getElementById("b1");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Abstract";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Hide abstract";
+    moreText.style.display = "inline";
+  }
+}
+
+function paper_abstract_2() {
+  var dots = document.getElementById("dots2");
+  var moreText = document.getElementById("abs2");
+  var btnText = document.getElementById("b2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Abstract";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Hide abstract";
+    moreText.style.display = "inline";
+  }
+}
+
+function paper_abstract_3() {
+  var dots = document.getElementById("dots3");
+  var moreText = document.getElementById("abs3");
+  var btnText = document.getElementById("b3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Abstract";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Hide abstract";
+    moreText.style.display = "inline";
+  }
+}
+
+function paper_abstract_4() {
+  var dots = document.getElementById("dots4");
+  var moreText = document.getElementById("abs4");
+  var btnText = document.getElementById("b4");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Abstract";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Hide abstract";
+    moreText.style.display = "inline";
+  }
+}
+
+function paper_abstract_5() {
+  var dots = document.getElementById("dots5");
+  var moreText = document.getElementById("abs5");
+  var btnText = document.getElementById("b5");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Abstract";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Hide abstract";
+    moreText.style.display = "inline";
+  }
+}
