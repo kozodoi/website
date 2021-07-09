c88ed871c3be445d46d747d9b9b894aeb3f081fa
kozodoi <kozodoi@users.noreply.github.com>
2021-07-09 09:00:52 +0000

    deploy: c360795ca8aca97c9ebd19cc1a54abdd3ab060be

--- assets/js/paper_description.js
+++ assets/js/paper_description.js
@@ -93,3 +93,67 @@ function paper_abstract_6() {
     moreText.style.display = "inline";
   }
 }
+
+function paper_abstract_7() {
+  var dots = document.getElementById("dots7");
+  var moreText = document.getElementById("abs7");
+  var btnText = document.getElementById("b7");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Abstract";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Hide abstract";
+    moreText.style.display = "inline";
+  }
+}
+
+function paper_abstract_8() {
+  var dots = document.getElementById("dots8");
+  var moreText = document.getElementById("abs8");
+  var btnText = document.getElementById("b8");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Abstract";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Hide abstract";
+    moreText.style.display = "inline";
+  }
+}
+
+function paper_abstract_9() {
+  var dots = document.getElementById("dots9");
+  var moreText = document.getElementById("abs9");
+  var btnText = document.getElementById("b9");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Abstract";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Hide abstract";
+    moreText.style.display = "inline";
+  }
+}
+
+function paper_abstract_10() {
+  var dots = document.getElementById("dots10");
+  var moreText = document.getElementById("abs10");
+  var btnText = document.getElementById("b10");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Abstract";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Hide abstract";
+    moreText.style.display = "inline";
+  }
+}
